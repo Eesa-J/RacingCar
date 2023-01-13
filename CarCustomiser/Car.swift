d72fa1f240f34f4d17c655e7f9c40ce11079085f
Eesa Jaswal <jaswal.e@etoncollege.org.uk>
2023-01-13 10:33:43 +0000

    Made stats display + tested it

---
+++ CarCustomiser/Car.swift
@@ -0,0 +1,21 @@
+//
+//  Car.swift
+//  CarCustomiser
+//
+//  Created by Jaswal, Eesa (SPH) on 13/01/2023.
+//
+
+import Foundation
+
+struct Car {
+    let make: String
+    let model: String
+    var topSpeed: Int
+    var acceleration: Double
+    var handling: Int
+    
+    func getStatsDisplay() -> String {
+        let statsDisplay = (" Make: \(make) \n Model: \(model) \n Top Speed: \(topSpeed)mph \n Acceleration (0-60): \(acceleration)s \n Handling: \(handling)")
+        return statsDisplay
+    }
+}
